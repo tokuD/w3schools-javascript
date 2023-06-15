52ab1f89644b5ac435cad48b7f9abfbbb3d64473
tokuD <megumu112851@gmail.com>
2023-06-15 23:59:51 +0900

    finish

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,8 @@
 "use strict";
+const input1 = document.querySelector('input[name="fname"]');
+const input2 = document.querySelector('input[name="lname"]');
+const button = document.querySelector('input[type="submit"]');
+console.log(input1.value);
+console.log(input2.value);
+console.log(button.value);
 //# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,15 @@
+const input1 = document.querySelector(
+  'input[name="fname"]'
+) as HTMLInputElement;
+
+const input2 = document.querySelector(
+  'input[name="lname"]'
+) as HTMLInputElement;
+
+const button = document.querySelector(
+  'input[type="submit"]'
+) as HTMLInputElement;
+
+console.log(input1.value);
+console.log(input2.value);
+console.log(button.value);
