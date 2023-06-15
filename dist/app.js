b757c5ce997c7d468fefce5e5efa6a48fd5c41e7
tokuD <megumu112851@gmail.com>
2023-06-16 00:05:37 +0900

    finish

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,6 @@
 "use strict";
+const body = document.querySelector("body");
+const p = document.createElement("p");
+body.appendChild(p);
+p.innerHTML = `Date: ${Date()}`;
 //# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+const body = document.querySelector("body")!;
+const p = document.createElement("p");
+body.appendChild(p);
+
+p.innerHTML = `Date: ${Date()}`;
+
