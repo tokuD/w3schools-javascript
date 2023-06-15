f2f417e54137745765a6286d342c0e9215f6dc63
tokuD <megumu112851@gmail.com>
2023-06-15 23:04:06 +0900

    reset app.ts

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,1 @@
 "use strict";
-const p = document.getElementById("demo");
-p.innerHTML = "Hello World";
-const body = document.querySelector("body");
-body.innerHTML = `
-<div>
-  <ul>
-    <li>item1</li>
-    <li>item2</li>
-    <li>item3</li>
-  </ul>
-</div>
-`;
